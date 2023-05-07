0ff605a97c044db89e7f10b9956c2b5dc4a2fdc0
Mina <mina9301@gmail.com>
2023-05-07 20:11:54 +0900

    chore: 설명 주석 추가

--- apps/wanted/next.config.js
+++ apps/wanted/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@wanted/ui'], // 추가
+  transpilePackages: ['@wanted/ui'], // 추가 - typescript를 javascript로 트랜스파일해주는 옵션
 };
 
 module.exports = nextConfig;
